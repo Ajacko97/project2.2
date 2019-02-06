0c0053a62454357f30c628c26d18544e800465dd
Ajacko97 <45338619+Ajacko97@users.noreply.github.com>
2019-02-06 15:38:36 +0100

    Changes 4

--- WeddingWWW/jQuery.js
+++ WeddingWWW/jQuery.js
@@ -6,11 +6,11 @@ $(document).ready(
 
             setInterval(function () {
                 $(".welcome h1").fadeOut(function () {
-                    if (!($(this).html === "Plan your dream wedding with us !")) {
-                        $(this).html("Plan your dream wedding with us !");
+                    if (!($(this).html === "Plan your dream wedding with us")) {
+                        $(this).html("Plan your dream wedding with us");
                         $(this).fadeIn();
-                    } else if (!($(this).html === "Plan your dream wedding with us !")) {
-                        $(this).html("Plan your dream wedding with us !");
+                    } else if (!($(this).html === "Plan your dream wedding with us")) {
+                        $(this).html("Plan your dream wedding with us");
                         $(this).fadeIn();
                     }
                 });
